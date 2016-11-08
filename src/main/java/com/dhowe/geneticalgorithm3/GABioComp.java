b8db0516d46e59543fb0434878ca7ac665695bff
Dpominic Howe <dominicjhowe@gmail.com>
2016-11-08 11:51:05 +0000

    cleaned up the main and output file information

--- src/main/java/com/dhowe/geneticalgorithm3/GABioComp.java
+++ src/main/java/com/dhowe/geneticalgorithm3/GABioComp.java
@@ -28,21 +28,21 @@ public class GABioComp { // Class
             int max_fitness = 1200; // we train on 1200 data points to classify 800 unseen data points
             int pop_size = 100;
             int gene_size = 130; // this is for 6 pairs of values and a output so 13 genes per rule and we ar getting 10 rules.
-            int prob = 10; // out of 1000 
+            int prob = 20; // out of 1000 
             int max_gen = 5000;
+            int target = max_fitness * pop_size;
 
-            String output = "";
             String graph_output = "";
 
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH,mm,ss");
             Calendar cal = Calendar.getInstance();
             String s = dateFormat.format(cal.getTime());
 
-            output += ("GA run on " + s + " \n Solving dataset2" + System.lineSeparator());
-
             GAEnviroment GA = new GAEnviroment(pop_size, gene_size, prob);
 
             int i = 0;
+            
+            graph_output += "Pop size " + pop_size + "," + "probability " + prob + " ," + "Gene size " + gene_size+ System.lineSeparator(); 
 
             while ((GA.main_population.getMax_fitness() < max_fitness) && (i <= max_gen)) { // keep going until max fitness is founf
 
@@ -51,33 +51,13 @@ public class GABioComp { // Class
                 int max_fit = GA.main_population.getMax_fitness();
                 float average = (float) GA.main_population.getAverage_fitness();
                 int pop_fit = GA.main_population.getPop_fitness();                
-                
-                output += ((i) + System.lineSeparator());
-                
+           
                 System.out.println((i) + " , " + max_fit + " , " + average);
-            //    System.out.println("Best :  " + GA.main_population.getPopulation()[GA.main_population.getBest_indices()].toString());
 
-            //    output += ("\t\tBest Fitness : " + max_fit + System.lineSeparator());
-            //    output += ("\t\tAverage Fitness : " + average + System.lineSeparator());
-
-                // keep a record of the best fitness and its generation
-//                if (bestpop < pop_fit) {
-//                    bestpop = pop_fit;
-//                    bestgen = i;
-//                }
-            //    output += ("\t\tPopulation Fitness : " + pop_fit
-            //            + " Fitness Target : " + (target)
-            //            + " Population Fitness Percentage : " + ((float) (pop_fit * 100) / target) + "%" + System.lineSeparator());
-
-            //    output += ("\t\tBest Population Fitness : " + bestpop + " Best Generation : " + bestgen + System.lineSeparator());
-
-            //    output += (GA.main_population.printPopulation() + System.lineSeparator());
-            //    output += (GA.main_population.toString() + System.lineSeparator());
-
-//                graph_output += (i + ", " + max_fit
-//                        + ", " + average
-//                        + ", " + pop_fit
-//                        + " ," + target + System.lineSeparator());
+                graph_output += (i + ", " + max_fit
+                        + ", " + average
+                        + ", " + pop_fit
+                        + " ," + target + System.lineSeparator());
 
                 i++;
             }
@@ -86,14 +66,11 @@ public class GABioComp { // Class
             int classified = GA.main_population.getPopulation()[GA.main_population.getBest_indices()].check_rules();
             System.out.println("Classified : " + classified + "  solved : "+ (float)((classified * 100) / 800) + "%" );
             
-            PrintWriter graphWriter = new PrintWriter("C:\\Users\\aphid\\Documents\\uniThirdYear\\Biocomputation\\Assignment\\results\\dataset3\\GAgraph+output-" + s + ".csv", "UTF-8");
 
-            try (PrintWriter writer = new PrintWriter("C:\\Users\\aphid\\Documents\\uniThirdYear\\Biocomputation\\Assignment\\results\\dataset3\\GAoutput-" + s + ".txt", "UTF-8")) {
+            try (PrintWriter graphWriter = new PrintWriter("C:\\Users\\aphid\\Documents\\uniThirdYear\\Biocomputation\\Assignment\\results\\dataset3\\GAgraph+output-" + s + ".csv", "UTF-8")) {
                 
-                writer.print(output);
                 graphWriter.print(graph_output);
                 graphWriter.close();
-                writer.close();
             }
             
         } catch (FileNotFoundException | UnsupportedEncodingException ex) {
